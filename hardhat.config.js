33fb914bf9ba6bc15613ad6b50a9018fcf1f257c
chinkauchenna2021 <uchennachinka4@gmail.com>
2023-05-23 16:29:01 +0100

    added seaport and uniswap

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,26 @@
 require("@nomicfoundation/hardhat-toolbox");
-
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.18",
+    solidity: {
+    compilers: [
+      {
+        version: "0.8.18",
+        settings: {
+          viaIR: false,
+          optimizer: {
+            enabled: false,
+          },
+        },
+        },
+        {
+        version:"0.7.6"
+      },
+    ],
+  },
+  networks: {
+    hardhat: {
+      blockGasLimit: 30_000_000,
+      throwOnCallFailures: false,
+    },
+  },
 };
